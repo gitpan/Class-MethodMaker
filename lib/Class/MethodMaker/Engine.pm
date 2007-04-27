d6df0fd6ddea38acc29ccb6df1cb8343b5d3ea6c
Steffen Schwigon <schwigon@cpan.org>
2007-04-27 07:40:18 +0000

    Import of SCHWIGON/Class-MethodMaker-2.10 from CPAN.
    gitpan-cpan-distribution: Class-MethodMaker
    gitpan-cpan-version:      2.10
    gitpan-cpan-path:         SCHWIGON/class-methodmaker/Class-MethodMaker-2.10.tar.gz
    gitpan-cpan-author:       SCHWIGON
    gitpan-cpan-maturity:     released

--- lib/Class/MethodMaker/Engine.pm
+++ lib/Class/MethodMaker/Engine.pm
@@ -57,7 +57,7 @@ BEGIN {
 # -------------------------------------
 
 our $PACKAGE = 'Class-MethodMaker';
-our $VERSION = '2.09';
+our $VERSION = '2.10';
 
 # -------------------------------------
 # CLASS CONSTRUCTION
